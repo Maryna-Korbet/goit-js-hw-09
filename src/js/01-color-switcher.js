0f029aa79393dc4812d97cc89ee8d30c24e6dd3c
Maryna-Korbet <m22v86k@gmail.com>
2022-08-05 23:09:43 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+// Write a script that, after clicking the "Start" button, changes the <body> background color once a second to a random value using the inline style. When clicking on the "Stop" button, background color change must stop.
+// Please note that the «Start» button can be clicked an infinite number of times. Make sure that the «Start» button is disabled while the theme change is running.
+// Use the getRandomHexColor function to generate a random color.
+
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+ 
+startBtn.addEventListener('click', () => {
+    startTimet();
+    startBtn.disabled = true; 
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function backgroundColorSwitcher() {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+}
+
+function startTimet() {
+    timerId = setInterval(backgroundColorSwitcher, 1000);
+}
 
